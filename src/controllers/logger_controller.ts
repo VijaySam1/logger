0ae457c6066654bef20a9f38b7ff57d02f681cb3
VijaySam1 <95839158+VijaySam1@users.noreply.github.com>
2023-01-03 16:27:47 +0530

    ee

--- src/controllers/logger_controller.ts
+++ src/controllers/logger_controller.ts
@@ -1,21 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { Request, Response, NextFunction } from 'express';
-import { LogType } from '../constants/Log_Type.enum';
-import { ActionKey } from '../constants/action_key.enum';
-import { GenericType } from '../constants/generic_type.enum';
-import LOGGER from '../models/logger';
-import { IError } from '../middleware/errorHandler';
-import moment from 'moment';
+import { Request, Response, NextFunction } from "express";
+import { LogType } from "../constants/Log_Type.enum";
+import { ActionKey } from "../constants/action_key.enum";
+import { GenericType } from "../constants/generic_type.enum";
+import LOGGER from "../models/logger";
+import { IError } from "../middleware/errorHandler";
+import moment from "moment";
 //req .body interface
 interface ILogger {
-  user_id: string, device_id: string, generic_id: string, generic_type: GenericType, action_key: ActionKey, action_value: string, log_type: LogType
+  user_id: string;
+  device_id: string;
+  generic_id: string;
+  generic_type: GenericType;
+  action_key: ActionKey;
+  action_value: string;
+  log_type: LogType;
 }
 // PaginationParams interface
 interface IPaginationParams {
-  from: string,
-  to: string,
-  page: number,
-  limit: number,
+  from: string;
+  to: string;
+  page: number;
+  limit: number;
 }
 
 // create a new log
@@ -27,32 +33,31 @@ export const postLog = (req: Request, res: Response, next: NextFunction) => {
     })
     .catch((error: IError) => {
       if (error.name == "ValidationError") {
-        next(new Error('Failed to add logger'));
-      } next(error);
+        next(new Error("Failed to add logger"));
+      }
+      next(error);
     });
-
 };
 
 //get log with pagination
 export const getLogs = (req: Request, res: Response, next: NextFunction) => {
-  
-  const requestQuery: IPaginationParams = req.query as unknown as IPaginationParams;
+  const requestQuery: IPaginationParams =
+    req.query as unknown as IPaginationParams;
   const { from, to, page, limit } = requestQuery;
 
   if (from && to && page && limit) {
-    const fromDate = new Date(from) ;
+    const fromDate = new Date(from);
     const toDate = new Date(to);
-    
-    const aggregateQuery = LOGGER.aggregate([{
-      $match:
+
+    const aggregateQuery = LOGGER.aggregate([
       {
-        createdAt:
-        {
-          '$gte': fromDate,
-          '$lte': toDate,
+        $match: {
+          createdAt: {
+            $gte: fromDate,
+            $lte: toDate,
+          },
         },
       },
-    },
     ]);
     const options = {
       page: page,
@@ -62,18 +67,14 @@ export const getLogs = (req: Request, res: Response, next: NextFunction) => {
       if (err) {
         next(err);
       } else {
-      res.status(200).json({ message: "success", data: result });
-      //res.render("logs",{result});
+        res.status(200).json({ message: "success", data: result });
+        //res.render("logs",{result});
       }
     });
   } else {
     res.status(422).send({ message: "Unprocessable entry" });
   }
-
 };
-
-
-
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
 // const fromDate = moment(req.query.from as string,'YYYY-MM-DD[T]HH:mm:ss.Dz SS:SS').format('YYYY-MM-DD[T]HH:mm:ss.Dz+SS:SS') ?? new Date().valueOf();
@@ -87,16 +88,20 @@ export const getLogs = (req: Request, res: Response, next: NextFunction) => {
 //   limit: parseInt(req.params.limit) || 10,
 // };
 
+// export const filter = (req: Request, res: Response, next: NextFunction) => {
+//   const requestBody: IPaginationParams =
+//     req.body as unknown as IPaginationParams;
+//   const { from, to, page, limit } = requestBody;
+//   const fromDate = new Date(from);
+//   const toDate = new Date(to);
 
-export const filter = (req: Request, res: Response, next: NextFunction) => {
-  const requestBody: IPaginationParams = req.body as unknown as IPaginationParams;
-  const { from, to, page, limit } = requestBody;
-  const fromDate=new Date(from);
-  const toDate=new Date(to);
-
-  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  res.status(300).redirect(`/api/log?from=${fromDate}&to=${toDate}&page=${page}&limit=${limit}`);
-}
+//   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+//   res
+//     .status(300)
+//     .redirect(
+//       `/api/log?from=${fromDate}&to=${toDate}&page=${page}&limit=${limit}`
+//     );
+// };
 
 // unction(request, response, next){
 
@@ -123,61 +128,61 @@ export const filter = (req: Request, res: Response, next: NextFunction) => {
 //       var column_sort_order = request.query.order[0]['dir'];
 //   }
 
-  //search data
+//search data
 
-  // var search_value = request.query.search['value'];
+// var search_value = request.query.search['value'];
 
-  // var search_query = `
-  //  AND (customer_first_name LIKE '%${search_value}%' 
-  //   OR customer_last_name LIKE '%${search_value}%' 
-  //   OR customer_email LIKE '%${search_value}%' 
-  //   OR customer_gender LIKE '%${search_value}%'
-  //  )
-  // `;
+// var search_query = `
+//  AND (customer_first_name LIKE '%${search_value}%'
+//   OR customer_last_name LIKE '%${search_value}%'
+//   OR customer_email LIKE '%${search_value}%'
+//   OR customer_gender LIKE '%${search_value}%'
+//  )
+// `;
 
-  // //Total number of records without filtering
+// //Total number of records without filtering
 
-  // database.query("SELECT COUNT(*) AS Total FROM customer_table", function(error, data){
+// database.query("SELECT COUNT(*) AS Total FROM customer_table", function(error, data){
 
-  //     var total_records = data[0].Total;
+//     var total_records = data[0].Total;
 
-  //     //Total number of records with filtering
+//     //Total number of records with filtering
 
-  //     database.query(`SELECT COUNT(*) AS Total FROM customer_table WHERE 1 ${search_query}`, function(error, data){
+//     database.query(`SELECT COUNT(*) AS Total FROM customer_table WHERE 1 ${search_query}`, function(error, data){
 
-  //         var total_records_with_filter = data[0].Total;
+//         var total_records_with_filter = data[0].Total;
 
-  //         var query = `
-  //         SELECT * FROM customer_table 
-  //         WHERE 1 ${search_query} 
-  //         ORDER BY ${column_name} ${column_sort_order} 
-  //         LIMIT ${start}, ${length}
-  //         `;
+//         var query = `
+//         SELECT * FROM customer_table
+//         WHERE 1 ${search_query}
+//         ORDER BY ${column_name} ${column_sort_order}
+//         LIMIT ${start}, ${length}
+//         `;
 
-  //         var data_arr = [];
+//         var data_arr = [];
 
-  //         database.query(query, function(error, data){
+//         database.query(query, function(error, data){
 
-  //             data.forEach(function(row){
-  //                 data_arr.push({
-  //                     'customer_first_name' : row.customer_first_name,
-  //                     'customer_last_name' : row.customer_last_name,
-  //                     'customer_email' : row.customer_email,
-  //                     'customer_gender' : row.customer_gender
-  //                 });
-  //             });
+//             data.forEach(function(row){
+//                 data_arr.push({
+//                     'customer_first_name' : row.customer_first_name,
+//                     'customer_last_name' : row.customer_last_name,
+//                     'customer_email' : row.customer_email,
+//                     'customer_gender' : row.customer_gender
+//                 });
+//             });
 
-  //             var output = {
-  //                 'draw' : draw,
-  //                 'iTotalRecords' : total_records,
-  //                 'iTotalDisplayRecords' : total_records_with_filter,
-  //                 'aaData' : data_arr
-  //             };
+//             var output = {
+//                 'draw' : draw,
+//                 'iTotalRecords' : total_records,
+//                 'iTotalDisplayRecords' : total_records_with_filter,
+//                 'aaData' : data_arr
+//             };
 
-  //             response.json(output);
+//             response.json(output);
 
-  //         });
+//         });
 
-  //     });
+//     });
 
-  // });
+// });
--- src/routes/logger_routes.ts
+++ src/routes/logger_routes.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { filter, getLogs, postLog } from "../controllers/logger_controller";
+import { getLogs, postLog } from "../controllers/logger_controller";
 import { loggerSchema } from "../schema/validation-schema";
 import { validateRequestBody } from "../middleware/val-error";
 import { methodNotFound } from "../middleware/errorHandler";
@@ -9,7 +9,6 @@ const router: Router = express.Router();
 router.get('/log', getLogs);
 router.post('/log', loggerSchema, validateRequestBody, postLog).all('/log',methodNotFound);
 
-router.post('/filter', filter);
 
 router.get('/get_data');
 
